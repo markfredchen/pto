731fbcf048c0d9aa923ba2fd0b6cdda8a0a04eba
Markfred Chen <markfred.chen@lanyon.com>
2015-05-26 11:01:01 +0800

    Update WeChat Validation

--- src/main/java/com/mcworkshop/pto/web/WeChatToken.java
+++ src/main/java/com/mcworkshop/pto/web/WeChatToken.java
@@ -1,15 +1,9 @@
 package com.mcworkshop.pto.web;
 
-import me.chanjar.weixin.mp.api.WxMpConfigStorage;
-import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
-import me.chanjar.weixin.mp.api.WxMpMessageRouter;
-import me.chanjar.weixin.mp.api.WxMpService;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 
 /**
