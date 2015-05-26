bb77b4b2c11085a5b9163005266c6839dcac1d11
Markfred Chen <markfred.chen@lanyon.com>
2015-05-26 10:59:32 +0800

    Update WeChat Validation

--- src/main/java/com/mcworkshop/pto/web/WeChatToken.java
+++ src/main/java/com/mcworkshop/pto/web/WeChatToken.java
@@ -1,12 +1,13 @@
 package com.mcworkshop.pto.web;
 
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
@@ -31,27 +32,10 @@ public class WeChatToken {
         return digest.equals(signature) ? echostr: "";
     }
 
-    @RequestMapping(value = "hello", method = RequestMethod.GET)
-    @ResponseBody
-    public String hello(HttpServletRequest request) {
-        String signature = request.getParameter("signature");
-        String echostr = request.getParameter("echostr");
-        String timestamp = request.getParameter("timestamp");
-        String nonce = request.getParameter("nonce");
-        System.out.println(request.getParameterMap().keySet());
-
-        return echostr;
-//        if(StringUtils.isEmpty(signature)
-//                || StringUtils.isEmpty(echostr)
-//                || StringUtils.isEmpty(timestamp)
-//                || StringUtils.isEmpty(nonce)) {
-//            return "invalid request";
-//        }else {
-////            String[] str = { TOKEN, timestamp, nonce };
-////            Arrays.sort(str);
-////            String bigStr = str[0] + str[1] + str[2];
-////            String digest = DigestUtils.sha1Hex(bigStr).toLowerCase();
-////            return digest.equals(signature) ? echostr: "error";
-//        }
+    @RequestMapping(value = "/weChat", method = RequestMethod.POST)
+    public @ResponseBody
+    String post(@RequestBody String requestBody) {
+        System.out.println(requestBody);
+        return "";
     }
 }
